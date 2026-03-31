76f35b0df306d5e250cbe041b8573d794503ee40
R-Crypt <R-Crypt@users.noreply.github.com>
2026-03-31 18:32:30 +0000

    deploy: b25f5e9947b55ca6822ab0f0d0ac966a117a58a8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1560451732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1831756244" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
